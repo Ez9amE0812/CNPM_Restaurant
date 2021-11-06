4af1605b751dde110690cf9be97f8ed89a2a169b
Toan Le <lopbona6ts@gmail.com>
2021-11-07 00:44:11 +0700

    some func

--- foodee/js/cart.js
+++ foodee/js/cart.js
@@ -14,7 +14,7 @@ if (document.readyState == 'loading') {
 }
 
 function ready() {
-    var removeCartItemButtons = document.getElementsByClassName('btn-remove')
+    var removeCartItemButtons = document.getElementsByClassName('btn-outline-danger')
     for (var i = 0; i < removeCartItemButtons.length; i++) {
         var button = removeCartItemButtons[i]
         button.addEventListener('click', removeCartItem)
@@ -26,81 +26,103 @@ function ready() {
         input.addEventListener('change', quantityChanged)
     }
 
-    var addToCartButtons = document.getElementsByClassName('shop-item-button')
+    var addToCartButtons = document.getElementsByClassName('btn-md')
     for (var i = 0; i < addToCartButtons.length; i++) {
         var button = addToCartButtons[i]
         button.addEventListener('click', addToCartClicked)
     }
 
-    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
 }
 
 
 function removeCartItem(event) {
     var buttonClicked = event.target
-    buttonClicked.parentElement.parentElement.remove()
+    buttonClicked.parentElement.parentElement.parentElement.remove()
     updateCartTotal()
 }
 
-function quantityChanged(event) {
-    var input = event.target
-    if (isNaN(input.value) || input.value <= 0) {
-        input.value = 1
+function quantitySub(event) {
+    var buttonClicked = event.target
+    var quantityControl = buttonClicked.parentElement.parentElement
+    var quantity = quantityControl.getElementsByClassName('cart-quantity')[0].innerText
+    if (quantity > 1) {
+            quantityControl.getElementsByClassName('cart-quantity')[0].innerText -= 1
     }
+    updateCartTotal()
+}
+
+function quantityAdd(event) {
+    var buttonClicked = event.target
+    var quantityControl = buttonClicked.parentElement.parentElement
+    var quantity = quantityControl.getElementsByClassName('cart-quantity')[0].innerText
+    quantityControl.getElementsByClassName('cart-quantity')[0].innerText = parseInt(quantity) + 1
     updateCartTotal()
 }
 
 function addToCartClicked(event) {
     var button = event.target
-    var shopItem = button.parentElement.parentElement
-    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
-    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
-    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    var shopItem = button.parentElement.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('card-text')[0].innerText
+    var price = shopItem.getElementsByClassName('price-text')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('card-img-top')[0].src
     addItemToCart(title, price, imageSrc)
     updateCartTotal()
 }
 
 function addItemToCart(title, price, imageSrc) {
-    var cartRow = document.createElement('div')
-    cartRow.classList.add('cart-row')
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    var cartRow = document.createElement('li')
+    cartRow.classList.add('cart__item')
+    var cartItems = document.getElementsByClassName('cart__list')[0]
+    var cartItemNames = cartItems.getElementsByClassName('cart__item-name')
     for (var i = 0; i < cartItemNames.length; i++) {
         if (cartItemNames[i].innerText == title) {
             alert('This item is already added to the cart')
             return
         }
     }
+
     var cartRowContents = `
-        <div class="row border-top border-bottom">
-            <div class="row main align-items-center">
-                <div class="col-2"><img class="img-fluid" src="images/res_img_1.jpg"></div>
-                <div class="col">
-                    <div class="row">Food name</div>
-                </div>
-                <div class="col"> <a href="#">-</a><a href="#" class="border">1</a><a href="#">+</a> </div>
-                <div class="col">&#36;44.00</div>
-                <div class="col"> <button class="btn-sm btn-outline-danger btn-del fs-4">&nbsp;X&nbsp;</button></div>
+            <div class="cart__item-img">
+                <img src="${imageSrc}" alt="">
             </div>
-        </div> `
+            <div class="cart__item-info">
+                <div class="cart__item-wrap">
+                    <h4 class="cart__item-name">${title}</h4>
+                    <button class="btn btn-outline-danger mx-1 cart__item-icon">&#10005;</button>
+                </div>
+                <div class="cart__item-wrap">
+                    <span class="cart__item-price">${price}</span>
+                    <div class="cart__item-control" onclick="">
+                        <div class="cart__item-control-wrap mx-1">
+                            <button class="btn-quantity btn-sub">-</button>
+                        </div>
+                        <span class="cart-quantity">1</span>
+                        <div class="cart__item-control-wrap mx-1">
+                            <button class="btn-quantity btn-add">+</button>
+                        </div>
+                    </div>
+                </div>
+            </div> `
     cartRow.innerHTML = cartRowContents
     cartItems.append(cartRow)
-    cartRow.getElementsByClassName('btn-del')[0].addEventListener('click', removeCartItem)
-    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+    cartRow.getElementsByClassName('btn-outline-danger')[0].addEventListener('click', removeCartItem)
+    cartRow.getElementsByClassName('btn-sub')[0].addEventListener('click', quantitySub)
+    cartRow.getElementsByClassName('btn-add')[0].addEventListener('click', quantityAdd)
 }
 
 function updateCartTotal() {
-    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
-    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var cartItemContainer = document.getElementsByClassName('cart__list')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart__item')
     var total = 0
     for (var i = 0; i < cartRows.length; i++) {
         var cartRow = cartRows[i]
-        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
-        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var priceElement = cartRow.getElementsByClassName('cart__item-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity')[0]
         var price = parseFloat(priceElement.innerText.replace('$', ''))
-        var quantity = quantityElement.value
+        var quantity = quantityElement.innerText
         total = total + (price * quantity)
     }
     total = Math.round(total * 100) / 100
     document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+    document.getElementsByClassName('cart-total-price')[1].innerText = '$' + total
 }
--- foodee/js/index/index.js
+++ foodee/js/index/index.js
@@ -2,7 +2,6 @@ let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
-    cart.classList.remove('active');
     loginForm.classList.remove('active');
     header__menu.classList.remove('active');
 }
@@ -21,7 +20,6 @@ let loginForm = document.querySelector('.login-form');
 document.querySelector('#login-btn').onclick = () =>{
     loginForm.classList.toggle('active');
     searchForm.classList.remove('active');
-    cart.classList.remove('active');
     header__menu.classList.remove('active');
 }
 
@@ -37,7 +35,6 @@ document.querySelector('#menu-btn').onclick = () =>{
 
 window.onscroll = () =>{
     searchForm.classList.remove('active');
-    cart.classList.remove('active');
     loginForm.classList.remove('active');
     header__menu.classList.remove('active');
 }
